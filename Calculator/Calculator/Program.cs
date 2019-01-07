8d73042408b99e538bc1da140ead0d395b1b974d
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-07 09:24:16 -0800

    Addition of add/subtract/multiply/divide

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,5 +8,23 @@ namespace Calculator
         {
             Console.WriteLine("Hello World!");
         }
+
+        //basic four-function functionality for the calculator
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static int Subtract(int a, int b)
+        {
+            return Add(a, -b);
+        }
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        static int Divide(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
